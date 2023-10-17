afb96736861d515ecc26b9c44135d2c9ee3c8dc3
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2023-10-17 17:54:29 +0100

    ori commit 1754

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./menu.module.css";
 import Link from "next/link";
 import Image from "next/image";
-
 import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
